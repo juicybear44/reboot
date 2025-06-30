db6c50c77ba527d4784550e22857bfd499929a4f
Sean Hong <dragonmoney44@proton.me>
2025-06-30 16:35:40 -0500

    removed react spring added motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import Clock from './Clock'
 import Carousel from './Carousel'
 
-const Hero = ({title='hello', subtitle='world'}) => {
+// import mountain from './assets/images/mountain1.png';
+// {title='hello', subtitle='world'}
+
+const Hero = () => {
   return (
-    <section className='bg-yellow-50 py-20'>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
-        <div className='text-center'>
-          <h1 className='text-4xl font-extrabold text-black sm:text-5xl md:text-6xl'>{title}</h1>
-          <p className='my-4 text-xl text-black'>{subtitle}</p>
-        </div>
-      <Clock/>
+    <section className='h-[300px] w-[2000] bg-yellow-50 py-20 bg-[url("./assets/images/campsite.png")] bg-contain bg-no-repeat bg-center overflow-auto'>
+      <div className='font-bold text-5xl translate-y-[-150%]'>
+        Welcome to the Den
       </div>
     </section>
   )
