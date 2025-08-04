f4f7a029c51905cf4fd52c8ae31d45dab99a9f31
Sean Hong <dragonmoney44@proton.me>
2025-08-04 04:58:50 -0500

    added bear to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import Clock from './Clock'
-import Carousel from './Carousel'
 
 
-import campsite from '../assets/images/campsite.png'
-// {title='hello', subtitle='world'}
-
+import campsite from '../assets/images/campsite.png';
+import bear from '../assets/images/mapbear.png';
 
 const Hero = () => {
   return (
     <section className='h-[300px] w-[2000] bg-yellow-50'>
       <div className="flex items-center justify-center">
+        <div
+          className='h-[125px] w-[125px] top-62'
+          style={{
+            backgroundImage: `url(${bear})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
+            position:'absolute',
+
+          }} 
+        ></div>
         <div className='font-bold text-5xl'>
           Welcome to
         </div>
